b676247e94a06dc715453a9699ab426dad9ad8ed
Wesley Soltanpur <zetialtd@gmail.com>
2019-01-18 12:37:05 +0000

    10% service charge when cold food ordered

--- src/main/scala/com/acme/cafex/Cafe.scala
+++ src/main/scala/com/acme/cafex/Cafe.scala
@@ -1,16 +1,29 @@
 package com.acme.cafex
 
+import scala.math.BigDecimal
+import scala.math.BigDecimal.RoundingMode
+
 object Cafe {
 
-  def itemPrice(item: String): Double = item match {
+  case class Bill(subTotal: BigDecimal, serviceCharge: BigDecimal, total: BigDecimal)
+
+  def itemPrice(item: String): BigDecimal = item match {
     case "Cola" => 0.5
     case "Coffee" => 1
     case "Cheese Sandwich" => 2
     case "Steak Sandwich" => 4.5
   }
 
-  def billFor(items: String*): Double = {
-    items.foldLeft[Double](0)((total, item) => total + itemPrice(item))
+
+  def billFor(items: String*): Bill = {
+    val subtotal = items.foldLeft[BigDecimal](0)((total, item) => total + itemPrice(item))
+
+    if (items.contains("Cheese Sandwich") || items.contains("Steak Sandwich")) {
+      val serviceCharge = (subtotal * 0.1).setScale(2, RoundingMode.HALF_EVEN)
+      return Bill(subtotal, serviceCharge, subtotal + serviceCharge)
+    }
+
+    Bill(subtotal, 0, subtotal)
   }
 
 }
--- src/test/scala/com/acme/cafex/CafeSuite.scala
+++ src/test/scala/com/acme/cafex/CafeSuite.scala
@@ -5,24 +5,31 @@ import org.scalatest.FunSuite
 
 class CafeSuite extends FunSuite {
   test("Cola price") {
-    assert(billFor("Cola") == 0.5)
+    assert(billFor("Cola").subTotal == 0.5)
   }
 
   test("Coffee price") {
-    assert(billFor("Coffee") == 1)
+    assert(billFor("Coffee").subTotal == 1)
   }
 
   test("Cheese sandwich price") {
-    assert(billFor("Cheese Sandwich") == 2)
+    assert(billFor("Cheese Sandwich").subTotal == 2)
   }
 
   test("Steak sandwich price") {
-    assert(billFor("Steak Sandwich") == 4.5)
+    assert(billFor("Steak Sandwich").subTotal == 4.5)
   }
 
   test("Mixed orders") {
-    assert(billFor("Cola", "Coffee") == 1.5)
-    assert(billFor("Cola", "Coffee", "Cheese Sandwich") == 3.5)
-    assert(billFor("Cola", "Coffee", "Cheese Sandwich", "Steak Sandwich") == 8)
+    assert(billFor("Cola", "Coffee").subTotal == 1.5)
+    assert(billFor("Cola", "Coffee", "Cheese Sandwich").subTotal == 3.5)
+    assert(billFor("Cola", "Coffee", "Cheese Sandwich", "Steak Sandwich").subTotal == 8)
+  }
+
+  test("10% service charge when food ordered") {
+    assert(billFor("Cheese Sandwich") == Bill(2, 0.2, 2.2))
+    assert(billFor("Steak Sandwich") == Bill(4.5, 0.45, 4.95))
+    assert(billFor("Cola", "Coffee", "Cheese Sandwich") == Bill(3.5, 0.35, 3.85))
+    assert(billFor("Cola", "Coffee", "Cheese Sandwich", "Steak Sandwich") == Bill(8, 0.8, 8.80))
   }
 }
